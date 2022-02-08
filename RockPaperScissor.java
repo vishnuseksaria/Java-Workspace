5ff9d506350e955947064d6716b83a2d0309e9af
Vinu-1792 <vishnuseksaria.vs@gmail.com>
2022-02-08 12:05:45 +0530

    RockPaparScissorGame

--- RockPaperScissor.java
+++ RockPaperScissor.java
@@ -1,15 +1,18 @@
 import java.util.*;
-public class RockPaperScissor {
-    public static void main(String[] args) {
+
+public class RockPaperScissor
+{
+    public static void main(String[] args)
+    {
         Scanner in = new Scanner(System.in);
         System.out.println("WELCOME\nThis is a rock paper scissor game.\nThere will be five rounds in this game.\nIf there is a tie, the round will not be counted.\nThe scores will be displayed after each round.\nThe one with greater score will be declared the winner.");
-//        System.out.println("Enter :\n1 for Rock\n2 for Paper\n3 for Scissor");
         int n=0,a=0,b=0;
         while(n!=5)
         {
             System.out.println("Enter :\n1 for Rock\n2 for Paper\n3 for Scissor");
             System.out.print("Player : ");
             double p = in.nextByte();
+
             if (p>3 || p<1)
             {
                 System.out.println("Invalid choice! Enter again.");
@@ -17,24 +20,30 @@ public class RockPaperScissor {
             }
             double c = Math.floor((Math.random()*3)+1);
             System.out.println("Computer : "+(int)c);
-            if(p==1 && c==2 || p==2 && c==3 || p==3 && c==1) {
+
+            if(p==1 && c==2 || p==2 && c==3 || p==3 && c==1)
+            {
                 a++;
                 System.out.println("Computer : "+a+" | Player : "+b);
             }
-            else if(c==1 && p==2 || c==2 && p==3 || c==3 && p==1) {
+            else if(c==1 && p==2 || c==2 && p==3 || c==3 && p==1)
+            {
                 b++;
                 System.out.println("Computer : "+a+" | Player : "+b);
             }
-            else{
+            else
+            {
                 System.out.println("Computer : "+a+" | Player : "+b);
                 continue;
             }
             n++;
         }
+
         if(a>b)
             System.out.println("Computer wins.");
         else
             System.out.println("Player wins.");
+
         System.out.println("ThankYou for using my game.");
     }
 }
